3e18d783f5f6ca341b3c6a728bde71c0c77ffbc8
Yash Yadav <yash9648444689@gmail.com>
2025-07-26 17:52:08 +0530

    Update caption_generator.py

--- caption_generator.py
+++ caption_generator.py
@@ -49,7 +49,7 @@ with st.sidebar:
 
 # --- Gemini API Setup ---
 try:
-    GEMINI_API_KEY = st.secrets["AlzaSyAOd68KONOGVNEyFk2A-ljSmr9Ena__Xbk"]
+    GEMINI_API_KEY = st.secrets["AIzaSyA7n8RSkyPM-s9p4DBfSVsIzzVmuKufeEE"]
     genai.configure(api_key=GEMINI_API_KEY)
     model = genai.GenerativeModel("models/gemini-1.5-flash")
 except Exception as e:
